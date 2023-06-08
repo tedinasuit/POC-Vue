b5dcca7234de9d32b30fad7d1a788e6fad5a38b2
Lars Hoeijmans <474593@student.fontys.nl>
2023-06-08 14:22:14 +0200

    Counter
    1st POC.

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        message: 'JQuery sucks!'
+    data() {
+        return {
+            value: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.value += 1;
+        }
     }
 
 })
